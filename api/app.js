18b6c95f321425c6500387942fa68ce5107d6f01
Renato <rniro811@mtroyal.ca>
2018-03-06 23:34:07 -0700

    user login functionality
    install crypto package
    complete login functionality
    remove .DS_Store from repo

--- api/app.js
+++ api/app.js
@@ -19,8 +19,7 @@ app.use(function(req,res,next){
 });
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const port = process.env.PORT || 3001;
--- api/controllers/users_controller.js
+++ api/controllers/users_controller.js
@@ -1,6 +1,14 @@
 import mongoose from 'mongoose';
 import User from '../models/users';
+import crypto from 'crypto';
 
 export const login = (req, res) => {
+  const md5 = crypto.createHash("md5");
 
+  User.findOne({ 'email': req.body.email }, 'id first_name last_name password').exec((err, usr) => {
+    if ( err || !usr || md5.update(req.body.password).digest("hex") !== usr.password )
+      return res.status(401).json( err || {} );
+    usr.password = undefined;
+    return res.json(usr);
+  });
 };
