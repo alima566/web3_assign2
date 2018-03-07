d4057fca048d89ad0e0dc74d7d1326faecd92f62
Aaron Li-Mai <aaronli-mai@Aarons-MacBook.local>
2018-03-07 15:05:25 -0700

    Fixed errors.

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,38 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-className Login extends Component {
+class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedIn: false
+      loggedIn: false,
+      username: '',
+      password: ''
     }
   }
 
   render() {
     return (
-      <div className="field">
-        <p className="control has-icons-left has-icons-right">
-          <input className="input" type="email" placeholder="Username" />
-          <span className="icon is-small is-left">
-            <i className="fas fa-user"></i>
-          </span>
-        </p>
-      </div>
+      <div>
+        <div className="field">
+          <p className="control has-icons-left has-icons-right">
+            <input className="input" type="email" placeholder="Username" />
+            <span className="icon is-small is-left">
+              <i className="fas fa-user"></i>
+            </span>
+          </p>
+        </div>
 
-      <div className="field">
-        <p className="control has-icons-left">
-          <input className="input" type="password" placeholder="Password" />
-          <span className="icon is-small is-left">
-            <i className="fas fa-lock"></i>
-          </span>
-        </p>
-      </div>
+        <div className="field">
+          <p className="control has-icons-left">
+            <input className="input" type="password" placeholder="Password" />
+            <span className="icon is-small is-left">
+              <i className="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
 
-      <div className="field">
-        <p className="control">
-          <button className="button is-success">Login</button>
-        </p>
+        <div className="field">
+          <p className="control">
+            <button className="button is-success">Login</button>
+          </p>
+        </div>
       </div>
     );
   }
