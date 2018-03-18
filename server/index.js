16fa6dd1cc4a1fc64bec313b74ca31b374fdc455
Renato <rniro811@mtroyal.ca>
2018-03-17 22:12:42 -0600

    main server modification + serve static files for heroku

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 require('babel-register')({ presets: ['es2015-node6'] });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 const express = require('express');
+const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 
 require('express-busboy').extend(app);
-app.use(express.static(require('path').join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.set('server', server);
 
 //Start API Server
@@ -14,6 +16,11 @@ require('./api/server.js')(app);
 
 //Create Socket.io Server and bind to main Server
 require('./chat/server.js')(app);
+
+//Forward all other routes to React build via index.html
+app.get('*', function(req, resp) {
+  resp.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 server.listen(port, () => {
   console.log(`API & Chat Server Listening at port ${port}`);
